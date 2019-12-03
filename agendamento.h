2a2bf287a804e6378c9d0892cf05c0ad55a07545
tlr08 <53915298+tlr08@users.noreply.github.com>
2019-12-03 13:33:33 -0300

    Update agendamento.h

--- agendamento.h
+++ agendamento.h
@@ -1,40 +1,31 @@
 #ifndef AGENDAMENTO_H_INCLUDED
 #define AGENDAMENTO_H_INCLUDED
 #include "stdafx.hpp"
-#include "cadastro.hpp"
 
 using namespace std;
 
 class agendamento{
 public:
 
-    string login[];
+    string login;
     string senha;
-
-    agendamento();
-    agendamento(string login, string senha);
-
-    string get_login()const;
-    string get_senha()const;
-
-    string set_login(string login);
-    string set_senha(string senha);
-
-
-};
-
-class agendamento::public cadastro(){
-private:
 
     string endereco;
     string data;
 
+    agendamento();
+    agendamento(string login, string senha, string endereco, string data);
+
+    string get_login()const;
+    string get_senha()const;
     string get_endereco()const;
-    string get_data()const; //como colocar a data?
+    string get_data()const;
 
+
+    string set_login(string login);
+    string set_senha(string senha);
     string set_endereco(string endereco);
-    string set_data(string data); //como colocar a data?
-
+    string set_data(string data);
 
 };
 #endif // AGENDAMENTO_H_INCLUDED
